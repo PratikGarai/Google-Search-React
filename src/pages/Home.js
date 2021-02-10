908a770ad565acca88d4a002ec48b43af54e04b2
PratikGarai <pratikgarai0208@gmail.com>
2021-02-10 20:53:01 +0530

    added few dependencies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,21 @@ import '../css/Home.css';
 const Home = (props) => {
     return (
         <div className="home">
-            This is home page
+            <h1>This is home page</h1>
+
+            <div className="home__header">
+                <div className="home__headerLeft">
+
+                </div>
+
+                <div className="home__headerRight">
+                    
+                </div>
+            </div>
+
+            <div className="home__body">
+
+            </div>
         </div>
     )
 }
