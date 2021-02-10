385180c22af825ca918b5108813578b57d756505
PratikGarai <pratikgarai0208@gmail.com>
2021-02-10 22:00:24 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './css/App.css';
 import Home from './pages/Home';
 import Result from './pages/Result';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Result />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            
+          </Route>
+        </Switch>
+        <Home />
+        <Result />
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import  AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
+
 import '../css/Home.css';
 
 const Home = (props) => {
     return (
         <div className="home">
-            <h1>This is home page</h1>
-
             <div className="home__header">
                 <div className="home__headerLeft">
-
+                    <Link to="/about">About</Link>
+                    <Link to="/store">Store</Link>
                 </div>
 
                 <div className="home__headerRight">
-                    
+                    <Link to="/gmail">Gmail</Link>
+                    <Link to="/images">Images</Link>
+                    <AppsIcon />
+                    <Avatar />
                 </div>
             </div>
 
