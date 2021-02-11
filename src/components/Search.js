8ce553134628facf9fc51d14d743e483d7f65c1a
PratikGarai <pratikgarai0208@gmail.com>
2021-02-11 21:00:37 +0530

    save point

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
+
 import '../css/Search.css'
 
 const Search = (props) => {
     return (
         <div className="search">
-            Search Component
+            <div className="search__input">
+                <SearchIcon className="search__inputIcon" />
+                <input />
+                <MicIcon />
+            </div>
+
+            <div className="search__buttons">
+                <Button variant="outlined" >
+                    Google Search
+                </Button>
+                <Button variant="outlined">
+                    I'm Feeling Lucky
+                </Button>
+            </div>
         </div>
     )
 }
