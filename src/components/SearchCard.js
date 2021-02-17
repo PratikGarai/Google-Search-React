742b09a750ea03d5e5f9fb8aee3e058dd58768ed
PratikGarai <pratikgarai0208@gmail.com>
2021-02-17 19:04:47 +0530

    some more stuff

---
+++ src/components/SearchCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchCard = (props) => {
+
+    return(
+        <div>
+            Item
+        </div>
+    );
+}
+
+export default SearchCard;
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,25 +1,85 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Search from '../components/Search';
+import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 import { useStateValue } from '../StateProvider';
 import useGoogleSearch from '../useGoogleSearch';
+import response from '../response';
+import SearchCard from '../components/SearchCard';
 
 import '../css/Result.css';
 
 const Result = (props) => {
 
     const [{term}, {}] = useStateValue();
-    const data = useGoogleSearch(term);
+    // const data = useGoogleSearch(term);
+    const data = response;
 
     console.log(data);
     return (
         <div className="search">
             <div className="searchPage__header">
+                <Link to="/">
+                    <img className="searchPage__logo" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="The Google Logo" />
+                </Link>
+                <div className="searchPage__header__body">
+                    <Search hideButtons />
+                    <div className="searchPage__options">
+                        <div className="searchPage__optionsLeft">
+                            <div className="searchPage__option">
+                                <SearchIcon />
+                                <Link to="/all">All</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <DescriptionIcon />
+                                <Link to="/news">News</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <ImageIcon />
+                                <Link to="/images">Images</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <LocalOfferIcon />
+                                <Link to="/shopping">Shopping</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <RoomIcon />
+                                <Link to="/maps">Maps</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <MoreVertIcon />
+                                <Link to="/more">More</Link>
+                            </div>
+                        </div>
+                        <div className="searchPage__optionsRight">
+                            <div className="searchPage__option">
+                                <Link to="/more">Settings</Link>
+                            </div>
+                            <div className="searchPage__option">
+                                <Link to="/more">Tools</Link>
+                            </div>
+                        </div>
 
+                    </div>
+                </div>
             </div>
+            
+            {term && (
+                <div className="searchPage__results">
+                    <p className="searchPage__resultCount">
+                        About {data?.searchInformation.formattedTotalResults} results ({data?.searchInformation.formattedSearchTime} seconds) for {term}
+                    </p>
 
-            <div className="searchPage__results">
-
-            </div>
+                    {data?.items.map(item => <SearchCard data={item} />)}
+                </div>
+                
+            )}
             This is result page for : {term}
         </div>
     );
